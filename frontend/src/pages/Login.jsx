1e147f3f96cf064ac32cd31e555b452a11311f8c
Pramish Maharjan <pramish143mhzn@gmail.com>
2025-11-12 20:29:42 +0545

    Day 8(Login Page): Login page design and successfully login.

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,12 +1,13 @@
 import React, { useContext, useState } from 'react'
 import { AuthContext } from '../context/AuthContext'
 import api from "../api/api";
-import { Link, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
     const { login } = useContext(AuthContext);
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
     const navigate = useNavigate();
 
     const handleSubmit = async (e) => {
@@ -17,19 +18,37 @@ const Login = () => {
         login(user, user.data.token);
         navigate("/");
       } catch (error) {
-        alert(error.response?.data?.message || "Login failed");
+        setError(error.response?.data?.message || "Login failed");
       }
     }
 
   return (
-    <div className='mx-auto flex flex-col justify-center items-center p-8 mt-35 border border-black bg-blue-100 rounded max-w-1/4'>
-        <h2 className='text-3xl font-bold mb-4'>Login</h2>
-        <form onSubmit={handleSubmit}>
-            <input className='border p-2 block mb-2' type='email' placeholder='Email' value={email} onChange={(e) => setEmail(e.target.value)} />
-            <input className='border p-2 block mb-2' type='password' placeholder='Password' value={password} onChange={(e) => setPassword(e.target.value)}/>
-            <button className='bg-blue-500 text-white px-4 py-2' type="submit">Login</button>
-            <Link className='ml-6 hover:font-bold text-blue-600 underline' to="/register">Register</Link>
+    <div className='flex items-center justify-center min-h-screen bg-gray-100 px-4'>
+      <div className='bg-white shadow-lg rounded-2xl p-8 w-full max-w-md'>
+        <h1 className='text-2xl font-bold text-center text-gray-800 mb-6'>Hospital Pathology Management System</h1>
+        <h2 className='text-lg font-semibold text-gray-700 text-center mb-6'>Login to your account</h2>
+        {error && (
+          <p className="text-red-500 bg-red-100 p-2 text-center rounded mb-4">
+            {error}
+          </p>
+        )}
+        <form onSubmit={handleSubmit} className='space-y-5'>
+          <div className='block text-gray-700 mb-1'>
+            <label htmlFor="">Email</label>
+            <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} className='w-full p-3 border border-gray-300 rounded-md focus:ring-2 focus:ring-blue-500 outline-none' required/>
+          </div>
+          <div className='block text-gray-700 mb-1'>
+            <label htmlFor="">Password</label>
+            <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} className='w-full p-3 border border-gray-300 rounded-md focus:ring-2 focus:ring-blue-500 outline-none' required/>
+          </div>
+          <button className='bg-blue-600 py-3 w-full hover:bg-blue-700 text-white rounded-md font-semibold transition'>Login</button>
         </form>
+
+        <p className="text-center text-sm text-gray-600 mt-6">
+          © {new Date().getFullYear()} Hospital Pathology Management System
+        </p>
+
+      </div>
     </div>
   )
 }
